80a1305268f546ed6ea5f6e08289de932a34e647
Jenna <jenna.maask@gmail.com>
2019-02-18 16:05:49 -0500

    cleaned up code

--- scripts./script.js
+++ scripts./script.js
@@ -1,7 +1,18 @@
+// variables
+const $beach = $(".beach");
+const $form = $("form");
+const $landing = $(".landing");
+const $lightning = $(".lightning");
+const $cloudy = $(".cloudy");
+const $waveSound = $(".wave-sound")[0];
+const $beerSound = $(".beers-sound")[0];
+const $cheersSound = $(".cheers-sound")[0];
+const $exit = $(".exit");
 
+// myApp object
 const myApp = {};
 
-//wave SVG's
+//wave SVG inputs
 myApp.waveGraphics = {
     flat: {
         amplitude1: 10,
@@ -20,7 +31,6 @@ myApp.waveGraphics = {
     }
 };
 
-
 //background choices
 myApp.background = {
     sunrise: "linear-gradient(to bottom, #ba5370, #f4e2d8)",
@@ -28,22 +38,20 @@ myApp.background = {
     night: "linear-gradient(to top, #283048, #859398)"
 };
 
+// value of delay between different sounds
 myApp.soundDelay = {
     boats: 10000,
     birds: 3000
-}
-
-
+};
 
 // event listener for submit
 myApp.init = () => {
-    $("form").on("submit", function(e){
+    $form.on("submit", function(e){
         e.preventDefault();
         myApp.determineUserInput();
-        $(".landing").hide();
-            // $(".beach").show();
-            myApp.displayExitIcon();
-            myApp.exit();
+        $landing.hide();
+        myApp.displayExitIcon();
+        myApp.exit();
     });
 };
 
@@ -52,13 +60,13 @@ myApp.determineUserInput = () => {
     const waveSize = $(`input[name="wave-size"]:checked`).val();
     const time = $(`input[name="time-of-day"]:checked`).val();
     const weather = $(`input[name="weather"]:checked`).val();
-    const sounds = ["birds", "boats", "beers"];
+    const sounds = ["birds", "beers", "boats"];
     const userSounds = sounds.filter((item) => {
         if ($(`input[name=${item}]`).prop("checked") == true) {
             return item;
         };
     });
-    $("audio").show();
+    $beach.css("height", "100vh");
     myApp.displayWave(waveSize);
     myApp.displayTimeAndWeather(time, weather);
     myApp.playSoundClips(weather, userSounds);
@@ -66,9 +74,6 @@ myApp.determineUserInput = () => {
 
 // display waves according to user size choice
 myApp.displayWave = (waveSize) => {
-    // $(".beach").append(myApp.waveGraphics[waveSize]);
-    // if (myApp.wave1 === undefined) {
-    $(".beach").css("height", "100vh");
     myApp.wave1 = $("#wave-1").wavify({
         height: 400,
         bones: 4,
@@ -89,105 +94,78 @@ myApp.displayWave = (waveSize) => {
         amplitude: myApp.waveGraphics[waveSize].amplitude3,
         speed: .25
     });
-// }else{
-//     myApp.wave1.reboot({
-//         height: 400,
-//         bones: 4,
-//         amplitude: myApp.waveGraphics[waveSize].amplitude1,
-//         speed: .15
-//     });
-//     myApp.wave2.reboot({
-//         height: 430,
-//         bones: 3,
-//         amplitude: myApp.waveGraphics[waveSize].amplitude2,
-//         speed: .20
-//     });
-//     myApp.wave3.reboot({
-//         height: 470,
-//         bones: 2,
-//         amplitude: myApp.waveGraphics[waveSize].amplitude3,
-//         speed: .25
-//     });
-// };
 };
 
 //display background according to user time and weather choice
 myApp.displayTimeAndWeather = (time, weather) => {
     if(weather === "sunny") {
-        $(".beach").css("background", myApp.background[time])
+        $beach.css("background", myApp.background[time])
     } else {
-        $(".cloudy").css({"background" : myApp.background[time], "display" : "block"});
-        $(".lightning").css({"display" : "block"});
+        $cloudy.css({"background" : myApp.background[time], "display" : "block"});
+        $lightning.css("display", "block");
     };
 };
 
-
 //play audio clips according to user choices
 myApp.playSoundClips = (weather, userSounds) => {
-    const thunder = $(".thunder-sound")[0]
-    $(".wave-sound")[0].play();
+    $waveSound.play();
     if(weather === "stormy") {
         setTimeout(() => {
-            thunder.play();
+            $(".thunder-sound")[0].play();
         }, 1000);
     };
     userSounds.forEach((item, index) => {
-        if (item === "beers") {
-            myApp.beerSoundEffect();
-        } else {
-            myApp.otherSoundEffects(item, index);
-        }
+            myApp.soundEffects(item, index);
     });
 };
 
 // beer sound effect 
 myApp.beerSoundEffect = () => {
-    $(".beers-sound")[0].play();
-            setTimeout(() => {
-                if (!$(".wave-sound")[0].paused && $(".wave-sound")[0].duration > 0){
-
-                    $(".cheers-sound")[0].play();
-                }
-            }, 3000);
-            $(".cheers-sound")[0].addEventListener("ended", function() {
+    $beerSound.play();
+    setTimeout(() => {
+        if (!$waveSound.paused && $waveSound.duration > 0){
+            $beerSound.play();
+        }
+    }, 3000);
+    $beerSound.addEventListener("ended", function() {
+        setTimeout(() => {
+            if (!$waveSound.paused && $waveSound.duration > 0){
+                $beerSound.play();
                 setTimeout(() => {
-                    if (!$(".wave-sound")[0].paused && $(".wave-sound")[0].duration > 0){
-                        $(".beers-sound")[0].play();
-                        setTimeout(() => {
-                            $(".cheers-sound")[0].play();
-                        }, 3000);
-                    }
-                    }, 15000);
-            });
-     
-        // };
-    // }, 1000);
-    
+                    $beerSound.play();
+                }, 3000);
+            }
+        }, 15000);
+    });   
 };
 
 // sound effects for options besides beer
-myApp.otherSoundEffects = (item, index) => {
+myApp.soundEffects = (item, index) => {
     const interval = 4000;
     setTimeout(() => {
-        if (!$(".wave-sound")[0].paused && $(".wave-sound")[0].duration > 0){
-            $(`.${item}-sound`)[0].play();
-        }
-    }, index * interval);
-    $(`.${item}-sound`)[0].addEventListener("ended", function() {
-        setTimeout(() => {
-            if (!$(".wave-sound")[0].paused && $(".wave-sound")[0].duration > 0){
-                $(`.${item}-sound`)[0].currentTime = 0;
+        if (!$waveSound.paused && $waveSound.duration > 0){
+            if (item === "beers") {
+                myApp.beerSoundEffect();
+            } else {
                 $(`.${item}-sound`)[0].play();
-
-            }
-        }, myApp.soundDelay[item]);
-    });
+                $(`.${item}-sound`)[0].addEventListener("ended", function() {
+                    setTimeout(() => {
+                        if (!$waveSound.paused && $waveSound.duration > 0){
+                            $(`.${item}-sound`)[0].currentTime = 0;
+                            $(`.${item}-sound`)[0].play();
+            
+                        };
+                    }, myApp.soundDelay[item]);
+                });
+            };
+        };
+    }, index * interval);
 };
 
 // insert exit svg into "exit" div
 myApp.displayExitIcon = () => {
-    $(".exit").html(`<svg class="exit-icon" xmlns="http://www.w3.org/2000/svg" xmlns: xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml: space="preserve"><path d="M24.762,79.088c0.507,0.508,1.173,0.762,1.838,0.762c0.665,0,1.331-0.254,1.838-0.762L50,57.527l21.562,21.562  c0.507,0.508,1.173,0.762,1.838,0.762c0.665,0,1.331-0.254,1.838-0.762c1.016-1.015,1.016-2.662,0-3.677L53.677,53.85l21.562-21.562  c1.016-1.015,1.016-2.662,0-3.677c-1.014-1.016-2.662-1.016-3.677,0L50,50.173L28.438,28.612c-1.014-1.016-2.662-1.016-3.677,0  c-1.016,1.015-1.016,2.662,0,3.677L46.324,53.85L24.762,75.412C23.746,76.427,23.746,78.073,24.762,79.088z" /></svg>`);
-}
+    $exit.html(`<svg class="exit-icon" xmlns="http://www.w3.org/2000/svg" xmlns: xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml: space="preserve"><path d="M24.762,79.088c0.507,0.508,1.173,0.762,1.838,0.762c0.665,0,1.331-0.254,1.838-0.762L50,57.527l21.562,21.562  c0.507,0.508,1.173,0.762,1.838,0.762c0.665,0,1.331-0.254,1.838-0.762c1.016-1.015,1.016-2.662,0-3.677L53.677,53.85l21.562-21.562  c1.016-1.015,1.016-2.662,0-3.677c-1.014-1.016-2.662-1.016-3.677,0L50,50.173L28.438,28.612c-1.014-1.016-2.662-1.016-3.677,0  c-1.016,1.015-1.016,2.662,0,3.677L46.324,53.85L24.762,75.412C23.746,76.427,23.746,78.073,24.762,79.088z" /></svg>`);
+};
 
 // on click remove all beach display and audio and return to landing page
 myApp.exit = () => {
@@ -195,22 +173,19 @@ myApp.exit = () => {
         myApp.wave1.kill();
         myApp.wave2.kill();
         myApp.wave3.kill();
-        $(".wave-graphic").each(() => {
-            // $(this).children("path").attr("d", "");
-            console.log($(this).children("path"));
-        });
-        $(".beach").css("height", "0px");
-        $(".landing").show();
-        $(".lightning").hide();
-        $(".cloudy").hide();
-        $(".beach").css("background", "none");
-        $(".exit").html("");
+        $beach.css("height", "0px");
+        $landing.show();
+        $lightning.hide();
+        $cloudy.hide();
+        $beach.css("background", "none");
+        $exit.html("");
         $("audio").each(function() {
             $(this)[0].pause();
+            $(this)[0].currentTime = 0;
         });
-        $("form")[0].reset();
-    })
-}
+        $form[0].reset();
+    });
+};
 
 $(function(){
     myApp.init();
